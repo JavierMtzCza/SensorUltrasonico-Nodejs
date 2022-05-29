18941c7d4091360b33a0a3d41975c8d7723f8734
Javatt <javier.martinez.carranzaipn.gmail>
2022-05-28 19:59:34 -0500

    Comentarios al codigo

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const { SerialPort } = require('serialport')
 const { ReadlineParser } = require('@serialport/parser-readline')
 
+// iniciamos conexion con nuestro arduino en el COM que le especificamos y a 9600 bauds
 const port = new SerialPort( 
     {
         path: 'COM4',
@@ -8,10 +9,15 @@ const port = new SerialPort(
     }
 )
 
+// Si hay un error esta funcion nos muestra que salio mal
 port.on('error', function(err) {
     console.log('Error: ', err.message)
 })
 
+// Este parser  nos lo transforma a string cada que encuentre un ~
 const parser = port.pipe(new ReadlineParser({ delimiter: '~' }))
-parser.on('data',(data)=>{console.log(data)})
+//cada que se detecte una recepcion, lo imprimimos en la consola
+parser.on('data',(data)=>{
+    console.log(data)
+})
 
